e9e799eade4a99e3ee461858e4534ab8e851ceda
kouta-UEHARA-Freeks <ueharakouta.cp@gmail.com>
2023-08-16 13:17:51 +0900

    Java4章課題_修正版
    一度提出したものを修正したものです。
    名前の表示を自分の名前にしました。

--- 4-1/src/check/Main.java
+++ 4-1/src/check/Main.java
@@ -5,36 +5,29 @@ import constants.Constants;
 public class Main {
     
     //課題①
-    private String firstName = "上原";
-    private String lastName = "孝太";
+    private String lastName = "上原";
+    private String firstName = "孝太";
     
     //課題②
-    private void printName(String firstName, String lastName) {
-        this.firstName = firstName;
+    private void printName(String lastName, String firstName) {
         this.lastName = lastName;
-        System.out.println("prinNameメソッド → " + this.firstName + this.lastName);
+        this.firstName = firstName;
+        
+        System.out.println("prinNameメソッド → " + this.lastName + this.firstName);
     }
     
     
-
     public static void main(String[] args) {
 
        Main main = new Main();
-       main.printName("七海", "真弥");
+       main.printName("上原", "孝太");
        
        Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
        pet.introduce();
 
        RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
        robotpet.introduce();
-       
-       
+              
     }
-
-    
-
-    
-    
-    
     
 }
